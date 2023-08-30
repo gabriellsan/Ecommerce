453f77c4c81801543f31a87193f2d0ea373190d0
Gabriel Santana Santos <gabrielsantana123santos@gmail.com>
2023-08-30 00:27:46 -0300

    correção catalogo produtos

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const catalogo = [
         marca: 'Zara', 
         nome: 'Camisa Larga com Bolsos', 
         preco: 70, 
-        imagem: 'product-1', 
+        imagem: 'product-1.jpg', 
         feminino: false, 
     }, 
     { 
@@ -12,7 +12,7 @@ const catalogo = [
         marca: 'Zara', 
         nome: 'Casaco Reto com Lã', 
         preco: 85, 
-        imagem: 'product-2', 
+        imagem: 'product-2.jpg', 
         feminino: true, 
     }, 
     { 
@@ -20,7 +20,7 @@ const catalogo = [
         marca: 'Zara', 
         nome: 'Jaqueta com Efeito Camurça', 
         preco: 60, 
-        imagem: 'product-3', 
+        imagem: 'product-3.jpg', 
         feminino: false, 
     },
     { 
@@ -28,7 +28,7 @@ const catalogo = [
         marca: 'Zara', 
         nome: 'Sobretudo em Mescla de Lã', 
         preco: 160, 
-        imagem: 'product-4', 
+        imagem: 'product-4.jpg', 
         feminino: false, 
     },
     { 
@@ -36,7 +36,7 @@ const catalogo = [
         marca: 'Zara', 
         nome: 'Camisa Larga Acolchoada de Veludo Cotelê', 
         preco: 110, 
-        imagem: 'product-5', 
+        imagem: 'product-5.jpg', 
         feminino: false,
     }, 
     { 
@@ -44,14 +44,14 @@ const catalogo = [
         marca: 'Zara', 
         nome: 'Casaco de Lã com Botões', 
         preco: 170, 
-        imagem: 'product-6', 
+        imagem: 'product-6.jpg', 
         feminino: true, }, 
     { 
         id: 7, 
         marca: 'Zara', 
         nome: 'Casaco com Botões', 
         preco: 75, 
-        imagem: 'product-7', 
+        imagem: 'product-7.jpg', 
         feminino: true, 
     }, 
     { 
@@ -59,7 +59,7 @@ const catalogo = [
         marca: 'Zara', 
         nome: 'Colete Comprido com Cinto', 
         preco: 88, 
-        imagem: 'product-8', 
+        imagem: 'product-8.jpg', 
         feminino: true, 
     }
 ];
