a69fc881d01ce00e644b584e94d37f09b8db9845
Gabriel Santana Santos <gabrielsantana123santos@gmail.com>
2023-08-30 00:21:08 -0300

    continuação

---
+++ main.js
@@ -0,0 +1,18 @@
+const catalogo = [
+    {
+        id: 1,
+        marca: "Zara",
+        nome:"Casaco Branco",
+        preco: 70,
+        nomeArquivoImagem: "product-1.jpg",
+    },
+    {
+        id: 2,
+        marca: "Zara",
+        nome:"Sobretudo azul marinho",
+        preco: 110,
+        nomeArquivoImagem: "product-2.jpg",
+    }
+];
+
+const cartaoProduto = ``;
