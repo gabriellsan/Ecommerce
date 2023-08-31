11f91b2f1a23ad248a3b6a1e004fcc619785e4b6
Gabriel Santana Santos <gabrielsantana123santos@gmail.com>
2023-08-31 10:56:49 -0300

    Adicionando produtos ao carrinho

--- src/cardProduto.js
+++ src/cardProduto.js
@@ -1,4 +1,5 @@
 import { catalogo } from "./utilidades";
+import { adicionarAoCarrinho } from "./menuCarrinho";
 
 export function renderizarCatalogo(){
     for(const produtoCatalogo of catalogo){
@@ -11,9 +12,12 @@ export function renderizarCatalogo(){
         <p class='text-sm'>${produtoCatalogo.marca}</p>
         <p class='text-sm'>${produtoCatalogo.nome}</p>
         <p class='text-sm'>$${produtoCatalogo.preco}</p>
-        <button class='bg-slate-950 hover:bg-slate-700 text-slate-200 '><i class="fa-solid fa-cart-plus"></i></button>
+        <button id='adicionar-${produtoCatalogo.id}' class='bg-slate-950 hover:bg-slate-700 text-slate-200 '><i class="fa-solid fa-cart-plus"></i></button>
         </div>`;
     
         document.getElementById("container-produto").innerHTML += cartaoProduto;
+    }
+    for(const produtoCatalogo of catalogo){
+        document.getElementById(`adicionar-${produtoCatalogo.id}`).addEventListener('click', () => adicionarAoCarrinho(produtoCatalogo.id));
     }
 }
--- src/menuCarrinho.js
+++ src/menuCarrinho.js
@@ -1,3 +1,5 @@
+import { catalogo } from "./utilidades";
+
 function abrirCarrinho() {
     document.getElementById('carrinho').classList.add('right-[0]');
     document.getElementById('carrinho').classList.remove('right-[-360px]');
@@ -16,16 +18,17 @@ export function inicializarCarrinho(){
     botaoAbrirCarrinho.addEventListener('click', abrirCarrinho);
 }
 
-export function adicionarAoCarrinho() {
+export function adicionarAoCarrinho(idProduto) {
+    const produto = catalogo.find(p => p.id === idProduto);
     const conteinerProdutosCarrinho = document.getElementById('produtos-carrinho');
    
     const cartaoProdutoCarrinho = `<article class="flex relative bg-slate-200 rounded-lg p-1">
     <button class="absolute top-0 right-2" id="fechar-carrinho"><i class="fa-solid fa-circle-xmark text-slate-500 hover:text-slate-800"></i></button>
-    <img class="h-24 rounded-lg" src="./assets/img/product-1.jpg" alt="Carrinho: camisa larga com bolsos">
+    <img class="h-24 rounded-lg" src="./assets/img/${produto.imagem}" alt="Carrinho:${produto.nome}">
     <div class="py-2"> <!--py === padding na vertical & px === padding na horizontal -->
-      <p class="text-slate-900 text-sm">camisa larga com bolsos</p>
+      <p class="text-slate-900 text-sm">${produto.nome}</p>
       <p class="text-slate-400 text-xs">Tamanho: M</p>
-      <p class="text-green-700 text-lg">$70</p>
+      <p class="text-green-700 text-lg">$${produto.preco}</p>
     </div>
   </article>`
 
