b2db1faa565ca526b423aa768f4ed30b3c7932f5
Gabriel Santana Santos <gabrielsantana123santos@gmail.com>
2023-08-30 23:37:27 -0300

    Código para fechar e abrir o carrinho de compras

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+import { inicializarCarrinho } from "./src/menuCarrinho";
+
 const catalogo = [
     { 
         id: 1, 
@@ -80,5 +82,5 @@ for(const produtoCatalogo of catalogo){
     document.getElementById("container-produto").innerHTML += cartaoProduto;
 }
 
-
+inicializarCarrinho();
 
